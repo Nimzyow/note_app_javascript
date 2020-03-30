50387bdf8c1460b8255a33bedeb533b3e1eb90d8
Nimzyow <n_soufiani@hotmail.com>
2020-03-30 19:27:57 +0100

    added document write up for each test

--- assert.js
+++ assert.js
@@ -1,33 +1,36 @@
 let assert = {
   isTrue: assertionToCheck => {
     if (!assertionToCheck) {
+      document.write("<p style='color: red; margin-top: 0px;'>failed</p>");
       throw new Error(
         "Assertion failed: " + assertionToCheck + "is not truthy"
       );
     } else {
-      return true;
+      document.write("<p style='color: green; margin-top: 0px;'>passed</p>");
     }
   },
   isSame: (one, two) => {
     if (one !== two) {
+      document.write("<p style='color: red; margin-top: 0px;'>failed</p>");
       throw new Error("Yo! Those aren't the same things!!!");
     } else {
-      console.log();
+      document.write("<p style='color: green; margin-top: 0px;'>passed</p>");
     }
   },
   includes: (obj, message) => {
     if (Object.values(obj).indexOf(message) > -1) {
       document.write("<p style='color: green; margin-top: 0px;'>passed</p>");
     } else {
-      document.write("<p style='color: green; margin-top: 0px;'>failed</p>");
+      document.write("<p style='color: red; margin-top: 0px;'>failed</p>");
       throw new Error(`Hey, hey, hey! your object doesnt include ${message}`);
     }
   },
   isSameLength: (one, two) => {
     if (one.length !== two.length) {
+      document.write("<p style='color: red; margin-top: 0px;'>failed</p>");
       throw new Error("Come on now, you know those two aren't the same length");
     } else {
-      return true;
+      document.write("<p style='color: green; margin-top: 0px;'>passed</p>");
     }
   }
 };
--- describe.js
+++ describe.js
@@ -1,3 +1,4 @@
+document.write("Nimzys testing framework! :) </br></br>");
 let describe = function(message, callBack) {
   document.write(message + "</br>");
   callBack();
